211cc024f8c0ff7cc14e9e553772a1d7edc7c3a2
Hidalgo Ponce <hidalgoponce@Hidalgos-MacBook-Pro.local>
2022-11-18 09:59:33 -0500

    Video update

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -301,7 +301,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
                     </svg>
-                    Headquarters in Cusco, offices in Lima, Arequipa and Tampa,FL.</p>
+                    Main Office : Cusco. Partner Offices: Buenos Aires, Rio de Janeiro, Quito, Santiago de Chile, Bogota, La Paz.</p>
                 <p class="flex my-4">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
