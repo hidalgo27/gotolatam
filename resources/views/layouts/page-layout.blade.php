0a4a376e8e948149da75c61bea660c9434e21cd5
Hidalgo Ponce <hidalgoponce@Hidalgos-MacBook-Pro.local>
2022-12-10 18:19:24 -0500

    About update

--- resources/views/layouts/page-layout.blade.php
+++ resources/views/layouts/page-layout.blade.php
@@ -160,7 +160,7 @@
 
         [
             'id' => '5',
-            'name' => __('message.nav_item5'),
+            'name' => 'FAQ',
             'state' => '3',
             'route' => route('about'),
             'active' => request()->routeIs('about'),
@@ -199,15 +199,6 @@
                             </svg>'
                         ],
                         [
-                            'name' => __('message.nav_sub_item4'),
-                            'state' => '3',
-                            'route' => route('about'),
-                            'active' => request()->routeIs('about'),
-                            'icon' => '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.3" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
-                            </svg>'
-                        ],
-                        [
                             'name' => __('message.nav_sub_item5'),
                             'state' => '3',
                             'route' => route('blog'),
@@ -228,11 +219,34 @@
                     ]
                 ]
 
+        ],
+
+        [
+            'id' => '3',
+            'name' => 'About Us',
+            'state' => '2',
+            'route' => route('about'),
+            'active' => request()->routeIs('about'),
+            'icon' => '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.3" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.3" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>',
+                    'nav_sub_links' =>[]
         ]
 
     ];
 
 @endphp
+{{--,--}}
+{{--[--}}
+{{--'name' => __('message.nav_sub_item4'),--}}
+{{--'state' => '3',--}}
+{{--'route' => route('about'),--}}
+{{--'active' => request()->routeIs('about'),--}}
+{{--'icon' => '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">--}}
+{{--    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.3" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />--}}
+{{--</svg>'--}}
+{{--],--}}
 {{--[--}}
 {{--'id' => '4',--}}
 {{--'name' => __('message.nav_item4'),--}}
