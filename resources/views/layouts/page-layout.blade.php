285ad84991a4e905f20afef978efc9a8dc169e58
Hidalgo Edely Chata Ponce <hidalgochponce@gmail.com>
2024-02-03 20:40:27 -0500

    Fusion realizadaODOD

--- resources/views/layouts/page-layout.blade.php
+++ resources/views/layouts/page-layout.blade.php
@@ -219,6 +219,16 @@
                 ]
 
         ],
+        [
+            'name' => __('message.nav_item6'),
+            'state' => '3',
+            'route' => route('reviews'),
+            'active' => request()->routeIs('reviews'),
+            'icon' => '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.3" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>',
+                    'nav_sub_links' => []
+        ],
 
         [
             'id' => '3',
